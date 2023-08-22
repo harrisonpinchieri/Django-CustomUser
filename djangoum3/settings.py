9e487f40f4904a00d7e84fc571f26db369a3d15f
harrison <harrisonpinchieri@gmail.com>
2023-08-22 14:46:00 -0300

    final

--- djangoum3/settings.py
+++ djangoum3/settings.py
@@ -124,6 +124,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'usuarios.CustomUsuario'
 
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'index'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
